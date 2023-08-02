f7dee7212fa2107867e1e865368b51090072b755
Jorge Vélez <jorgeluisvelez@gmail.com>
2023-08-02 11:09:25 -0700

    Update video-recorder.js

--- src/video-recorder.js
+++ src/video-recorder.js
@@ -33,15 +33,12 @@ const CONSTRAINTS = {
 }
 
 const Wrapper = styled.div`
-  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 100%;
-  overflow: hidden;
-  min-height: 300px;
   background-color: #000;
   color: white;
   box-sizing: border-box;
